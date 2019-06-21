cd57942144d9299e1aa7183f2e5e727d55af26c3
rigwild <contact@asauvage.fr>
2019-06-21 22:31:32 +0200

    Update examples

--- examples/basic/generate.js
+++ examples/basic/generate.js
@@ -7,7 +7,7 @@ const r = require('path').resolve
 const config = {
   path: r(__dirname), // Path to apiDoc data directory
   output: r(__dirname, 'example.md'), // Output path
-  template: r(__dirname, '../../templates', 'default.md'), // Template path
+  template: r(__dirname, '..', '..', 'templates', 'default.md'), // Template path
   prepend: null, // File to preprend documentation with
   multi: false, // Should the documentation be generated one file per group
   createPath: false // Should the path to output path be recursively generated (mkdir -p)
