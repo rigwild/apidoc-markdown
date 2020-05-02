175c6aaecac50c032fbf7f77b04f87d7d7f81230
rigwild <me@rigwild.dev>
2020-05-02 18:46:07 +0200

    Move examples to example

--- test/tests.ts
+++ test/tests.ts
@@ -2,7 +2,7 @@ import test from 'ava'
 import fs from 'fs'
 import path from 'path'
 
-const examples = path.resolve(__dirname, '..', 'examples')
+const example = path.resolve(__dirname, '..', 'example')
 const templates = path.resolve(__dirname, '..', 'templates')
 
 test.serial('The default template should be present', t => t.true(fs.existsSync(path.resolve(templates, 'default.md'))))
@@ -11,21 +11,21 @@ test.serial('Example apiDoc data should be present and valid json', t => {
   const files = ['api_data.json', 'api_project.json']
   t.plan(files.length * 2)
   files.forEach(file => {
-    const filePath = path.resolve(examples, '_apiDocData', 'out', file)
+    const filePath = path.resolve(example, '_apiDocData', 'out', file)
     t.true(fs.existsSync(filePath))
     t.notThrows(() => require(filePath))
   })
 })
 
-const runGenerator = async (example: string) => (await import(path.resolve(examples, example, 'generate'))).default()
+const runGenerator = async (example: string) => (await import(path.resolve(example, example, 'generate'))).default()
 test.serial('`basic` example generation should not fail', async t => await t.notThrowsAsync(runGenerator('basic')))
 test.serial('`multi` example generation should not fail', async t => await t.notThrowsAsync(runGenerator('multi')))
 
-test.serial('`example.md` should be present in examples/basic', async t =>
-  t.true(fs.existsSync(path.resolve(examples, 'basic', 'example.md')))
+test.serial('`example.md` should be present in example/basic', async t =>
+  t.true(fs.existsSync(path.resolve(example, 'basic', 'example.md')))
 )
 
-test.serial('Documentation files should be present in examples/multi/output', async t => {
+test.serial('Documentation files should be present in example/multi/output', async t => {
   const files = [
     'Define.md',
     'Deprecated.md',
@@ -41,5 +41,5 @@ test.serial('Documentation files should be present in examples/multi/output', as
     'indent.md'
   ]
   t.plan(files.length)
-  files.forEach(file => t.true(fs.existsSync(path.resolve(examples, 'multi', 'output', file))))
+  files.forEach(file => t.true(fs.existsSync(path.resolve(example, 'multi', 'output', file))))
 })
