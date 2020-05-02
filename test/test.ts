92b0ba05f3be79aadc130a6e3347cb06c9007942
rigwild <me@rigwild.dev>
2020-05-02 19:18:28 +0200

    Upgrade tests

--- test/test.ts
+++ test/test.ts
@@ -1,28 +1,28 @@
 import test from 'ava'
 import fs from 'fs'
-import path from 'path'
+import { resolve as r } from 'path'
 
-const example = path.resolve(__dirname, '..', 'example')
-const templates = path.resolve(__dirname, '..', 'templates')
+const exampleDir = r(__dirname, '..', 'example')
+const templates = r(__dirname, '..', 'templates')
 
-test.serial('The default template should be present', t => t.true(fs.existsSync(path.resolve(templates, 'default.md'))))
+test.serial('The default template should be present', t => t.true(fs.existsSync(r(templates, 'default.md'))))
 
 test.serial('Example apiDoc data should be present and valid json', t => {
   const files = ['api_data.json', 'api_project.json']
   t.plan(files.length * 2)
   files.forEach(file => {
-    const filePath = path.resolve(example, '_apiDocData', 'out', file)
+    const filePath = r(exampleDir, '_apiDocData', 'out', file)
     t.true(fs.existsSync(filePath))
     t.notThrows(() => require(filePath))
   })
 })
 
-const runGenerator = async (example: string) => (await import(path.resolve(example, example, 'generate'))).default()
+const runGenerator = async (selected: string) => (await import(r(exampleDir, selected, 'generate.ts'))).default()
 test.serial('`basic` example generation should not fail', async t => await t.notThrowsAsync(runGenerator('basic')))
 test.serial('`multi` example generation should not fail', async t => await t.notThrowsAsync(runGenerator('multi')))
 
 test.serial('`example.md` should be present in example/basic', async t =>
-  t.true(fs.existsSync(path.resolve(example, 'basic', 'example.md')))
+  t.true(fs.existsSync(r(exampleDir, 'basic', 'example.md')))
 )
 
 test.serial('Documentation files should be present in example/multi/output', async t => {
@@ -41,5 +41,5 @@ test.serial('Documentation files should be present in example/multi/output', asy
     'indent.md'
   ]
   t.plan(files.length)
-  files.forEach(file => t.true(fs.existsSync(path.resolve(example, 'multi', 'output', file))))
+  files.forEach(file => t.true(fs.existsSync(r(exampleDir, 'multi', 'output', file))))
 })
