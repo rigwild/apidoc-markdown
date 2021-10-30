b4032085f3e7e7be0f885eb3de2538be1fb3e2af
rigwild <me@rigwild.dev>
2021-10-30 15:42:14 +0200

    Simplify doc

--- src/cli.ts
+++ src/cli.ts
@@ -2,29 +2,26 @@ import yargs from 'yargs'
 import { ConfigurationObjectCLI } from './types'
 
 const cli = yargs
-  .usage('Generate Markdown documentation from apiDoc data.')
+  .usage('Generate a Simple and Portable Markdown documentation for your API.')
   .usage(
     'Usage: apidoc-markdown -i <path> -o <output_file> [-t <template_name>] [--multi] [--createPath] [--prepend <file_path>]'
   )
-  .example('apidoc-markdown -i src/ -o doc.md', 'Generate from `src/` source files to `./doc.md`')
-  .example('apidoc-markdown --input src --output doc.md', 'Generate from `src/` source files to `./doc.md`')
+  .example('apidoc-markdown -i src -o doc.md', 'Generate from `src` source files to `doc.md`')
+  .example('apidoc-markdown --input src --output doc.md', 'Generate from `src` source files to `doc.md`')
   .example(
-    'apidoc-markdown -i src/ -o doc.md -t bitbucket',
-    'Generate from `src/` source files to `./doc.md` using the `bitbucket` template'
+    'apidoc-markdown -i src -o doc.md -t bitbucket',
+    'Generate from `src` source files to `doc.md` using the `bitbucket` template'
   )
   .example(
-    'apidoc-markdown -i src/ -o doc.md -t ./mytemplate.md',
-    'Generate from `src/` source files to `./doc.md` using a provided template file'
+    'apidoc-markdown -i src -o doc.md -t my_custom_template.md',
+    'Generate from `src` source files to `doc.md` using a provided template file'
   )
-  .example(
-    'apidoc-markdown -i src -o doc --multi --createPath',
-    'Generate from `src/` source files to `./doc/<group>.md`'
-  )
+  .example('apidoc-markdown -i src -o doc --multi', 'Generate from `src` source files to `doc/<group>.md`')
   .option('input', {
     alias: 'i',
     demandOption: true,
     describe: 'Input source files path',
-    default: './src',
+    default: 'src',
     type: 'string'
   })
   .option('output', {
@@ -35,8 +32,7 @@ const cli = yargs
   })
   .option('template', {
     alias: 't',
-    describe:
-      'Name of the template to be used (`default`, `bitbucket`) or path to an EJS template file. If not specified, the default template is used',
+    describe: 'Name of the template to be used (`default`, `bitbucket`) or path to an EJS template file.',
     default: 'default',
     type: 'string'
   })
